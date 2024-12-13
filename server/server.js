df4d1735e97f9ee7e3c79f675e2807a97d0bdf71
soumytiwari <tiwarisoumya111@gmail.com>
2024-12-13 19:54:34 +0530

    added database

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import { config } from 'dotenv'; // or import dotenv/config.js
 import dotenv from 'dotenv';
-import { port } from './config.js';
+import { port, mongoDBURL } from './config.js';
 
 const app = express()
 app.use(cors())         // enable CORS for all origins
@@ -11,12 +11,19 @@ app.use(express.json())         // parse incoming req with JSON payloads.
 
 dotenv.config()
 
+// create http route
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Welcome!')
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-}
-)
+// connect database
+mongoose.connect(mongoDBURL).then(() => {
+    console.log('App connected to database')
+    // listen port
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
+}).catch((error) => {
+    console.log(error)
+})
