54532a30b3ee09eff332ec7e59e35308d15bd96a
soumytiwari <tiwarisoumya111@gmail.com>
2024-12-13 20:15:48 +0530

    created task table in database

---
+++ server/models/Task.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+// module.exports = mongoose.model('Task', taskSchema)
+export const Task = mongoose.model('Task', taskSchema)
