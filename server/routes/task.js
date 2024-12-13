156ce51f72e0bca081200fcd3f04f1c4c50e2d28
soumytiwari <tiwarisoumya111@gmail.com>
2024-12-13 22:56:45 +0530

    added read one task

--- server/routes/task.js
+++ server/routes/task.js
@@ -21,6 +21,53 @@ router.post('/', async (req, res) => {
 })
 
 // GET/tasks (Read All)
-// router.get
+router.get('/', async (req, res) => {
+    try {
+        const tasks = await Task.find()
+        res.status(200).json({
+            count: tasks.length,
+            data: tasks
+        })
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json({
+            message: err.message
+        })
+    }
+})
+
+// GET/task/:id (Read One)
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const task = await Task.findById(id)
+        res.status(200).json(task)
+    } catch (err) {
+        console.log(err.message)
+        res.status(400).json({ message: err.message })
+    }
+})
+
+// PUT/tasks/:id (Update)
+router.put('/:id', async(req, res) => {
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true} )
+        res.status(200).json(updatedTask)
+    } catch (err) {
+        console.log(err.message)
+        res.status(400).json({ message:err.message})
+    }
+})
+
+// DELETE/tasks/:id (Delete)
+router.delete('/:id', async (req, res) => {
+    try {
+        await Task.findByIdAndDelete(req.params.id)
+        res.status(204).json({ message: 'Task deleted'})
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json({ message: err.message })
+    }
+})
 
 export default router;
