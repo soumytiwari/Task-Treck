33214c15f620d299bc0effe0b7e7495ec1c058fc
soumytiwari <tiwarisoumya111@gmail.com>
2024-12-15 20:01:29 +0530

    added the routes for each tasks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateTask from './pages/CreateTask'
+import ShowBook from './pages/ShowBook'
+import EditTask from './pages/EditTask'
+import DeleteTask from './pages/DeleteTask'
+import DeleteAllTask from './pages/DeleteAllTask'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>
-      App
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/tasks/create' element={<CreateTask/>} />
+      <Route path='/tasks/details/:id' element={<ShowBook/>} />
+      <Route path='/tasks/edit/:id' element={<EditTask/>} />
+      <Route path='/tasks/delete/:id' element={<DeleteTask/>} />
+      <Route path='/tasks/delete' element={<DeleteAllTask/>} />
+    </Routes>
   )
 }
 
