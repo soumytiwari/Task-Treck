bc677cee92bd33ac79ad10db76b68e2a31b0d78c
soumytiwari <tiwarisoumya111@gmail.com>
2024-12-16 01:29:47 +0530

    can create task

--- client/src/pages/CreateTask.jsx
+++ client/src/pages/CreateTask.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const CreateTask = () => {
+  const [title, setTitle] = useState("");
+  // const [user, setUser] = useState('')
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSaveTask = () => {
+    const data = {
+      title,
+      // user,
+      description,
+      date,
+    };
+    setLoading(true);
+    axios
+      .post("http://localhost:3000/tasks/", data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("An error happened. Please check console");
+        console.log(err);
+      });
+  };
   return (
-    <div>
-      Create
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Create Task</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label htmlFor="" className="text-xl mr-4 text-gray-500">
+            Title
+          </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label htmlFor="" className="text-xl mr-4 text-gray-500">Description</label>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label htmlFor="" className="text-xl mr-4 text-gray-500">Date</label>
+          <input
+            type="text"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveTask}>
+          Save
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateTask
+export default CreateTask;
